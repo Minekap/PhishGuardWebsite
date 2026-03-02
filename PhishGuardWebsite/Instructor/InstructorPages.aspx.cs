b21b6f8aeee1122ced5136a89358f6b5d08376d5
butwhytho098 <chenghao2468@gmail.com>
2026-03-02 16:09:20 +0800

    Add InstructorPages with associated files and updates
    - Updated solution file to include project ID.
    - Created new ASP.NET page `InstructorPages.aspx` with basic structure.
    - Added code-behind file `InstructorPages.aspx.cs` with `InstructorPages` class.
    - Generated designer file `InstructorPages.aspx.designer.cs` for form control.
    - Added multiple branches for feature development in the repository.

---
+++ PhishGuardWebsite/Instructor/InstructorPages.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PhishGuardWebsite.Instructor
+{
+    public partial class InstructorPages : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ s
@@ -0,0 +1,8 @@
+  [31morigin/HEAD[m -> origin/master
+  [31morigin/dev[m
+  [31morigin/feature/admin-pages[m
+  [31morigin/feature/guest-auth[m
+  [31morigin/feature/instructor-pages[m
+  [31morigin/feature/interactive-phuah[m
+  [31morigin/feature/member-pages[m
+  [31morigin/master[m
